df580f1d1fa920fa4bb610cb5b1fd7205d2827ec
Farrel Athalla Putra <13523118@std.stei.itb.ac.id>
2025-07-24 08:50:59 +0700

    [1004] [Time Beats: 5.42%] [Memory Beats: 25.66%] - LeetPush

---
+++ Max-Consecutive-Ones-III.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int max = 0;
+        int curr = 0;
+        queue<int> temp;
+        for (int i = 0; i < nums.size(); i++) {
+            temp.push(nums[i]);
+            if (nums[i] == 0) {
+                if (k > 0) {
+                    k--;
+                } else {
+                    if (curr > max) max = curr;
+                    while (temp.front() != 0) {
+                        temp.pop();
+                        curr--;
+                    }
+                    temp.pop();
+                    curr--;
+                }
+            }
+            curr++;
+        }
+        if (curr > max) max = curr;
+        return max;
+    }
+};
